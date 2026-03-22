b64f15dba9444b92fb5be98f73ea1527c93e2930
Moonflute <96515859+Moonflute@users.noreply.github.com>
2026-03-23 00:28:32 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('appetite-bell-store').then((cache) => cache.addAll([
+      './',
+      './index.html',
+      './manifest.json',
+      './icon-192.png',
+      './icon-512.png',
+      './quotes.txt'
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
